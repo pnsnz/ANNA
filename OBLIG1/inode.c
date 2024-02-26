14685c591ef32b9cc0bba4b7c9b3f27d4df77a67
hypogirl5000 <annsor@hotmail.no>
2024-02-26 19:44:02 +0100

    Update inode.c
    fiksa litt på load_inodes mtp laste inn navnet.

--- OBLIG1/inode.c
+++ OBLIG1/inode.c
@@ -6,21 +6,11 @@
 #include <string.h>
 #include <errno.h>
 
-/* The number of bytes in a block.
- * Do not change.
- */
+
 #define BLOCKSIZE 4096
 
-/* The lowest unused node ID.
- * Do not change.
- */
 static int num_inode_ids = 0;
 
-/* This helper function computes the number of blocks that you must allocate
- * on the simulated disk for a give file system in bytes. You don't have to use
- * it.
- * Do not change.
- */
 static int blocks_needed( int bytes )
 {
     int blocks = bytes / BLOCKSIZE;
@@ -29,12 +19,6 @@ static int blocks_needed( int bytes )
     return blocks;
 }
 
-/* This helper function returns a new integer value when you create a new inode.
- * This helps you do avoid inode reuse before 2^32 inodes have been created. It
- * keeps the lowest unused inode ID in the global variable num_inode_ids.
- * Make sure to update num_inode_ids when you have loaded a simulated disk.
- * Do not change.
- */
 static int next_inode_id( )
 {
     int retval = num_inode_ids;
@@ -80,21 +64,69 @@ int delete_file( struct inode* parent, struct inode* node )
 
 int delete_dir( struct inode* parent, struct inode* node )
 {
+    // når slettet skal det ikke være noen filer i dir!
     /* to be implemented */
     return 0;
 }
 
 struct inode* load_inodes( char* master_file_table )
 {
-    /* to be implemented */
-    return NULL;
+    FILE *fil = fopen(master_file_table, "r");
+    if (fil == NULL) {
+        perror("File opening");
+        return EXIT_FAILURE;
+    }
+
+    // struct
+    // kanskje ikke smart å kalle den root mtp den skal være generell?
+    struct inode* root = malloc(sizeof(struct inode));
+
+    // ID
+    fread(&root->id, 1, sizeof(int),fil);
+
+    //LENGTH OF NAME
+    int length_of_name;
+    fread(&length_of_name, 1, sizeof(int), fil);
+
+    // ->/.? 
+    root->name = malloc(length_of_name + 1); //+1 for good meassure?
+    if (root->name == NULL){
+        printf("heap alloc failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    // leser fra fil inn i struct navn
+    fread(root->name, 1, length_of_name, fil);
+
+    //flag
+    fread(&root->is_directory, 1, sizeof(char), fil);
+
+    // dom c syntax ugh
+    if (root->is_directory) {
+        //num_children
+        fread(&root->num_children, 1, sizeof(int), fil);
+
+        root->children = malloc(sizeof(struct inode*) * (root->num_children));
+
+        for (int i = 0; i < root->num_children; i++) {
+            fread(&root->children[i], 1, sizeof(size_t), fil);
+            root->children[i] = load_inodes(master_file_table); //?
+        }
+    }
+    else {
+        fread(&root->filesize, 1, sizeof(int), fil); //file_size
+        fread(&root->num_blocks, 1, sizeof(int), fil); //num_blocks
+
+        root->blocks = malloc(sizeof(size_t) * root->num_blocks);
+
+        fread(&root->blocks, sizeof(size_t), root->num_blocks, fil);
+    }
+
+    fclose(fil);
+
+    return EXIT_SUCCESS;
 }
 
-/* The function save_inode is a recursive functions that is
- * called by save_inodes to store a single inode on disk,
- * and call itself recursively for every child if the node
- * itself is a directory.
- */
 static void save_inode( FILE* file, struct inode* node )
 {
     if( !node ) return;
@@ -152,13 +184,8 @@ void save_inodes( char* master_file_table, struct inode* root )
     fclose( file );
 }
 
-/* This static variable is used to change the indentation while debug_fs
- * is walking through the tree of inodes and prints information.
- */
 static int indent = 0;
 
-/* Do not change.
- */
 void debug_fs( struct inode* node )
 {
     if( node == NULL ) return;
@@ -187,8 +214,6 @@ void debug_fs( struct inode* node )
     }
 }
 
-/* Do not change.
- */
 void fs_shutdown( struct inode* inode )
 {
     if( !inode ) return;
